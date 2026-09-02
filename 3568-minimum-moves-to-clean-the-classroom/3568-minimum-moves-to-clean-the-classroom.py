e080c369c8350d3d0a0226488dd5f4e46d38e677
Banty Kumar <158995119+Banty825413@users.noreply.github.com>
2026-09-02 14:00:44 +0530

    Time: 2120 ms (68.42%), Space: 60.1 MB (66.67%) - LeetHub

--- 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.py
+++ 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.py
@@ -1,63 +1,68 @@
 class Solution:
     def minMoves(self, classroom: List[str], energy: int) -> int:
-        m, n = len(classroom), len(classroom[0])
-        litter_positions = []
+        m , n = len(classroom) , len(classroom[0])
         start = None
+        litter_pos = []
 
         for i in range(m):
-            for j in range(n):
-                c = classroom[i][j]
-                if c == 'L':
-                    litter_positions.append((i, j))
-                elif c == 'S':
-                    start = (i, j)
+            for j in range (n):
 
-        k = len(litter_positions)
-        litter_index = {pos: idx for idx, pos in enumerate(litter_positions)}
+                if classroom[i][j] == "S":
+                    start = (i,j)
+                elif classroom[i][j] =="L":
+                    litter_pos.append((i,j))
+        
+        k = len(litter_pos)
         full_mask = (1 << k) - 1
-
-        sr, sc = start
+        # Making any dic to store the litter position with their index value in mask
+        litter_index = {pos : idx for idx, pos  in enumerate (litter_pos)}
+        sr , sc = start 
         start_mask = 0
-        if (sr, sc) in litter_index:
-            start_mask |= (1 << litter_index[(sr, sc)])
-
+        if (sr, sc ) in litter_index :
+            start_mask = start_mask | (1 << litter_index[(sr,sc)])
+        
         if start_mask == full_mask:
             return 0
+        
+        visited = {(sr, sc , start_mask) : energy}  # dic where L is alredy picked with energy level
 
-        visited = {(sr, sc, start_mask): energy}
-        q = deque([(sr, sc, energy, start_mask, 0)])  # row, col, energy, mask, moves
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        q = deque([(sr,sc,energy,start_mask , 0)]) # It carry cordinates of row , energy lvl , bit mask for    
+                                                    #that co ordianate , and distance it travel
 
+        direction = [(-1,0),(1,0),(0,1),(0,-1)]
         while q:
-            r, c, e, mask, moves = q.popleft()
 
-            if e == 0:
-                continue  # stuck, can't move further
+            r , c , e, mask , dist = q.popleft()
 
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-                if not (0 <= nr < m and 0 <= nc < n):
+            if e == 0 :
+                continue
+            for dr , dc in direction:
+                nr , nc = dr +r , dc + c
+
+                if not( 0 <= nr < m  and 0 <= nc < n):
                     continue
-                if classroom[nr][nc] == 'X':
+                if classroom[nr][nc] == "X":
                     continue
-
-                ne = e - 1
+                ne = e -1 
                 nmask = mask
                 cell = classroom[nr][nc]
 
-                if cell == 'L' and (nr, nc) in litter_index:
-                    nmask = mask | (1 << litter_index[(nr, nc)])
-                if cell == 'R':
-                    ne = energy  # reset to max capacity
+                if cell == "L":
+                    nmask = mask | (1 << litter_index[(nr,nc)])
+                if cell =="R":
+                    ne = energy 
+                key = (nr,nc,nmask)
 
-                key = (nr, nc, nmask)
                 if key in visited and visited[key] >= ne:
-                    continue
+                    continue 
                 visited[key] = ne
 
                 if nmask == full_mask:
-                    return moves + 1
+                    return dist + 1
 
-                q.append((nr, nc, ne, nmask, moves + 1))
+                q.append((nr,nc , ne,nmask , dist + 1))             
+
+
+
 
         return -1
