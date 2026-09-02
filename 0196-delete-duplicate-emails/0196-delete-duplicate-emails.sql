ca0e2934af5e68db6cb6cf38cc5e053d6e51cfd2
Banty Kumar <158995119+Banty825413@users.noreply.github.com>
2026-09-02 14:52:14 +0530

    Time: 1001 ms (29.12%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,5 +1,6 @@
-# Write your MySQL query statement below
-delete p1 
-from Person p1 ,
-Person p2
-where p1.email = p2.email and p1.id > p2.id
+with sametable as ( select p1.id from person p1 
+                join  person p2
+                on p1.email = p2.email
+                where p1.id > p2.id
+                )
+delete from person where id  in (select id from sametable)
